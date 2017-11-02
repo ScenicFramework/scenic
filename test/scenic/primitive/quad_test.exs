59735980e0b48f9ab9cbfc7a9c0d642db3d49355
Boyd Multerer <boyd@multerer.com>
2017-11-02 11:32:02 -0700

    different types of quads

--- test/scenic/primitive/quad_test.exs
+++ test/scenic/primitive/quad_test.exs
@@ -11,28 +11,32 @@ defmodule Scenic.Primitive.QuadTest do
   alias Scenic.Primitive.Quad
 
 
-  @data     {{100,300},{300,180},{400,310},{300,520}}
+  @convex       {{100,300},{300,180},{400,310},{300,520}}
+  @concave      {{100,300},{300,180},{400,310},{300,200}}
+  @degenerate   {{100,300},{400,100},{400,300},{100,100}}
 
 
   #============================================================================
   # build / add
 
   test "build works" do
-    p = Quad.build( @data )
+    p = Quad.build( @convex )
     assert Primitive.get_parent_uid(p) == -1
     assert Primitive.get_module(p) == Quad
-    assert Primitive.get(p) == @data
+    assert Primitive.get(p) == @convex
   end
 
 
   #============================================================================
   # verify
 
-  test "verify passes valid data" do
-    assert Quad.verify( @data ) == true
+  test "verify passes valid convex" do
+    assert Quad.verify( @convex )     == true
+    assert Quad.verify( @concave )    == true
+    assert Quad.verify( @degenerate ) == true
   end
 
-  test "verify fails invalid data" do
+  test "verify fails invalid convex" do
     assert Quad.verify( {{10,11}, 40, 80, 666} )   == false
     assert Quad.verify( {10, 40, 80} )             == false
     assert Quad.verify( {{10,11,12}, 40, 80} )     == false
@@ -52,15 +56,15 @@ defmodule Scenic.Primitive.QuadTest do
   # transform helpers
 
   test "default_pin returns the averaged center of the rect" do
-    assert Quad.default_pin(@data) == {275, 328}
+    assert Quad.default_pin(@convex) == {275, 328}
   end
 
 #  test "centroid returns the center of the rect" do
-#    assert Quad.centroid(@data) == {30, 52}
+#    assert Quad.centroid(@convex) == {30, 52}
 #  end
 
-  test "expand expands the data" do
-    {{x0,y0},{x1,y1},{x2,y2},{x3,y3}} = Quad.expand(@data, 10)
+  test "expand expands the convex" do
+    {{x0,y0},{x1,y1},{x2,y2},{x3,y3}} = Quad.expand(@convex, 10)
     # rounding to avoid floating-point errors from messing up the tests
     assert {
       {round(x0), round(y0)},
@@ -73,17 +77,17 @@ defmodule Scenic.Primitive.QuadTest do
   #============================================================================
   # point containment
   test "contains_point? returns true if it contains the point" do
-    assert Quad.contains_point?(@data, {101, 300})  == true
-    assert Quad.contains_point?(@data, {300,181})   == true
-    assert Quad.contains_point?(@data, {399,310})   == true
-    assert Quad.contains_point?(@data, {300,519})   == true
+    assert Quad.contains_point?(@convex, {101, 300})  == true
+    assert Quad.contains_point?(@convex, {300,181})   == true
+    assert Quad.contains_point?(@convex, {399,310})   == true
+    assert Quad.contains_point?(@convex, {300,519})   == true
   end
 
   test "contains_point? returns false if the point is outside" do
-    assert Quad.contains_point?(@data, {100, 180}) == false
-    assert Quad.contains_point?(@data, {400, 180}) == false
-    assert Quad.contains_point?(@data, {400, 520}) == false
-    assert Quad.contains_point?(@data, {100, 520}) == false
+    assert Quad.contains_point?(@convex, {100, 180}) == false
+    assert Quad.contains_point?(@convex, {400, 180}) == false
+    assert Quad.contains_point?(@convex, {400, 520}) == false
+    assert Quad.contains_point?(@convex, {100, 520}) == false
   end
 
   #============================================================================
@@ -100,12 +104,12 @@ defmodule Scenic.Primitive.QuadTest do
       300  :: integer-size(16)-native,
       520  :: integer-size(16)-native,
     >>
-    assert Quad.serialize(@data)           == {:ok, native}
-    assert Quad.serialize(@data, :native)  == {:ok, native}
+    assert Quad.serialize(@convex)           == {:ok, native}
+    assert Quad.serialize(@convex, :native)  == {:ok, native}
   end
 
   test "serialize big works" do
-    assert Quad.serialize(@data, :big) == {:ok, <<
+    assert Quad.serialize(@convex, :big) == {:ok, <<
       100  :: integer-size(16)-big,
       300  :: integer-size(16)-big,
       300  :: integer-size(16)-big,
@@ -128,8 +132,8 @@ defmodule Scenic.Primitive.QuadTest do
       300  :: integer-size(16)-native,
       520  :: integer-size(16)-native,
     >>
-    assert assert Quad.deserialize(bin)          == {:ok, @data, ""}
-    assert assert Quad.deserialize(bin, :native) == {:ok, @data, ""}
+    assert assert Quad.deserialize(bin)          == {:ok, @convex, ""}
+    assert assert Quad.deserialize(bin, :native) == {:ok, @convex, ""}
   end
 
   test "deserialize big works" do
@@ -143,7 +147,7 @@ defmodule Scenic.Primitive.QuadTest do
       300  :: integer-size(16)-big,
       520  :: integer-size(16)-big,
     >>
-    assert assert Quad.deserialize(bin, :big) == {:ok, @data, ""}
+    assert assert Quad.deserialize(bin, :big) == {:ok, @convex, ""}
   end
 
 end
