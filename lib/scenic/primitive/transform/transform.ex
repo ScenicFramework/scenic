e6e8c9ad37b001ea6bcadea53de85ff41e77072a
Boyd Multerer <boyd@multerer.com>
2017-11-02 10:00:18 -0700

    cleanup and fix transform tests

--- lib/scenic/primitive/transform/transform.ex
+++ lib/scenic/primitive/transform/transform.ex
@@ -34,90 +34,26 @@ defmodule Scenic.Primitive.Transform do
     end # quote
   end # defmacro
 
-  #--------------------------------------------------------
-  def info(), do: "Scenic.Primitive.Transform is not meant to be used directly."
-
-  #--------------------------------------------------------
-  def verify(_), do: false
-
-
-#  #===========================================================================
-#  def get(transform_map, transform_type)
-#
-#  def get(txs, :pin) do
-#    Map.get(txs, :pin)
-#  end
-#
-#  def get(txs, :matrix) do
-#    Map.get(txs, :matrix)
-#  end
-#
-#  def get(txs, :rotate) do
-#    Map.get(txs, :rotate)
-#  end
-#
-#  def get(txs, :scale) do
-#    Map.get(txs, :scale)
-#  end
-#
-#  def get(txs, :translate) do
-#    Map.get(txs, :translate)
-#  end
-#
-#  def get(_, type) do
-#    raise "Attempted to get a transform of type #{inspect(type)}.\n" <>
-#      "The only allowed transform types are :pin, :matrix, :rotate, :scale, and :translate"
-#  end
-
-  #===========================================================================
-#  def put(transform_map, transform_type, data)
-#
-#  def put(txs, type, nil) do
-#    Map.delete(txs, type)
-#  end
-#
-#  def put(txs, :pin, data) do
-#    Scenic.Primitive.Style.Transform.Pin.verify!(data)
-#    Map.put(txs, :pin, data)
-#  end
-#
-#  def put(txs, :matrix, data) do
-#    Scenic.Primitive.Style.Transform.Matrix.verify!(data)
-#    Map.put(txs, :matrix, data)
-#  end
-#
-#  def put(txs, :rotate, data) do
-#    Scenic.Primitive.Style.Transform.Rotate.verify!(data)
-#    Map.put(txs, :rotate, data)
-#  end
-#
-#  def put(txs, :scale, data) do
-#    Scenic.Primitive.Style.Transform.Scale.verify!(data)
-#    Map.put(txs, :scale, data)
-#  end
-#
-#  def put(txs, :translate, data) do
-#    Scenic.Primitive.Style.Transform.Translate.verify!(data)
-#    Map.put(txs, :translate, data)
-#  end
-#
-#  def put(_, type, _) do
-#    raise "Attempted to put a transform of type #{inspect(type)}.\n" <>
-#      "The only allowed transform types are :pin, :matrix, :rotate, :scale, and :translate"
-#  end
-
-
-
 
   #============================================================================
   # transform helper functions
 
+  #--------------------------------------------------------
   def calculate_local( txs )
 
   def calculate_local( nil ), do: nil
   def calculate_local( txs ) when (txs == %{}), do: nil
   
-  def calculate_local( txs ) do
+  def calculate_local( %{pin: pin} = txs ) do
+  # look for case where only the pin is set
+    case Enum.count(txs) do
+      1 -> nil
+      _ -> do_calculate_local( txs )
+    end
+  end
+  def calculate_local( txs ), do: do_calculate_local( txs )
+  
+  defp do_calculate_local( txs ) do
     # start with identity - which is like multiplying by 1
     @identity
     |> multiply_partial( :matrix, txs[:matrix] )
@@ -125,6 +61,7 @@ defmodule Scenic.Primitive.Transform do
     |> rotate_and_scale( txs )
   end
 
+  #--------------------------------------------------------
   defp multiply_partial(mx, type, value)
 
   defp multiply_partial(mx, _, nil), do: mx
@@ -138,7 +75,7 @@ defmodule Scenic.Primitive.Transform do
     Matrix.translate( mx, Vector.invert( point ) )
   end
 
-
+  #--------------------------------------------------------
   defp rotate_and_scale( mx, txs ) do
     # don't do any work if neight rotate nor scale are set
     # don't need to translate twice for no reason
@@ -154,29 +91,5 @@ defmodule Scenic.Primitive.Transform do
   end
 
 
-  #============================================================================
-  # calculate full matrix based on a stack of matrixes up the graph#
-
-  alias Scenic.Graph
-  alias Scenic.Primitive
-
-  def calc_final_matrix(graph, primitive) do
-    gather_matrix_list(graph, primitive)
-    |> Matrix.mul()
-  end
-
-  def gather_matrix_list(graph, primitive, list \\ [])
-  def gather_matrix_list(_, nil, list), do: list
-  def gather_matrix_list(graph, primitive, list) do
-    parent = Graph.get(graph, Primitive.get_parent_uid(primitive))
-
-    Primitive.get_transforms( primitive )
-    |> calculate_local()
-    |> case do
-      nil -> gather_matrix_list(graph, parent, list)
-      mx  -> gather_matrix_list(graph, parent, [ mx | list])
-    end
-  end
-  
 
 end
--- test/scenic/primitive/transform_test.exs
+++ test/scenic/primitive/transform_test.exs
@@ -22,235 +22,13 @@ defmodule Scenic.Primitive.TransformTest do
   @translate    {4,5}
   @mx           MatrixBin.build_rotation(-0.2)
 
-  @tx           Transform.build(
-      pin:    @pin,
-      rot:    @rotation,
-      scl:    @scale,
-      trans:  @translate,
-      mx:     @mx
-    )
-
-  @dflag      3
-  @mx_code    0x01
-
-  #============================================================================
-  # def build( opts )
-
-  test "build sets the pin" do
-    assert Transform.get_pin(@tx) == @pin
-  end
-
-  test "build sets the rotation" do
-    assert Transform.get_rotation(@tx) == @rotation
-  end
-
-  test "build sets the scale" do
-    assert Transform.get_scale(@tx) == @scale
-  end
-
-  test "build sets the translation" do
-    assert Transform.get_translation(@tx) == @translate
-  end
-
-  test "build sets the matrix" do
-    assert Transform.get_matrix(@tx) == @mx
-  end
-  
-  test "build calculates the final matrix" do
-    {_,_,_,_,_,fin} = @tx
-    assert is_binary(fin) == true
-  end
-
-
-  #============================================================================
-  # put( transform, type, value)
-
-  test "put clears final matrix" do
-    {_,_,_,_,_,nil} = Transform.put(@tx, :pin, {123,456})
-    {_,_,_,_,_,nil} = Transform.put(@tx, :rot, 1)
-    {_,_,_,_,_,nil} = Transform.put(@tx, :scale, 1.1)
-    {_,_,_,_,_,nil} = Transform.put(@tx, :trans, {1,2})
-    {_,_,_,_,_,nil} = Transform.put(@tx, :mx, nil)
-  end
-
-  test "put nils the value given a nil value" do
-    {nil,_,_,_,_,nil} = Transform.put(@tx, :pin, nil)
-    {_,nil,_,_,_,nil} = Transform.put(@tx, :rot, nil)
-    {_,_,nil,_,_,nil} = Transform.put(@tx, :scale, nil)
-    {_,_,_,nil,_,nil} = Transform.put(@tx, :trans, nil)
-    {_,_,_,_,nil,nil} = Transform.put(@tx, :mx, nil)
-  end
-
-  test "put builds a new tuple if transform is nil" do
-    {@pin,nil,nil,nil,nil,_}        = Transform.put(nil, :pin, @pin)
-    {nil,@rotation,nil,nil,nil,_}   = Transform.put(nil, :rot, @rotation)
-    {nil,nil,@scale,nil,nil,_}      = Transform.put(nil, :scale, @scale)
-    {nil,nil,nil,@translate,nil,_}  = Transform.put(nil, :trans, @translate)
-    {nil,nil,nil,nil,@mx,_}         = Transform.put(nil, :mx, @mx)
-  end
-
-  test "put sets the named value" do
-    mx = MatrixBin.build_scale(1.01)
-
-    {{11,22},_,_,_,_,_}   = Transform.put(@tx, :pin, {11,22})
-    {_,{0.1,:x},_,_,_,_}  = Transform.put(@tx, :rot, {0.1,:x})
-    {_,_,3.3,_,_,_}       = Transform.put(@tx, :scale, 3.3)
-    {_,_,_,{1,2},_,_}     = Transform.put(@tx, :trans, {1,2})
-    {_,_,_,_,^mx,_}       = Transform.put(@tx, :mx, mx)
-  end
-
-
-  #============================================================================
-  # pin
-
-  #--------------------------------------------------------
-  test "get_pin returns the pin value" do
-    assert Transform.get_pin(@tx) == @pin
-  end
-
-  test "get_pin returns nil given nil transform" do
-    assert Transform.get_pin(nil) == nil
-  end
-
-  #--------------------------------------------------------
-  test "put_pin updates the pin data and calculates new final" do
-    {_,_,_,_,_,old_fin} = @tx
-    {{11,22},_,_,_,_,new_fin} = Transform.put_pin(@tx, {11,22})
-    refute new_fin == old_fin
-  end
-
-
-  #============================================================================
-  # rotation
-  
-  #--------------------------------------------------------
-  test "get_rotation returns the rotation data" do
-    assert Transform.get_rotation(@tx) == @rotation
-  end
-
-  test "get_rotation returns nil given nil transform" do
-    assert Transform.get_rotation(nil) == nil
-  end
-
-  #--------------------------------------------------------
-  test "put_rotation updates the rotation and calculates new final" do
-    {_,_,_,_,_,old_fin} = @tx
-    {_,{0.1,:x},_,_,_,new_fin} = Transform.put_rotation(@tx, {0.1,:x})
-    refute new_fin == old_fin
-  end
-
-
-  #============================================================================
-  # scale
-  
-  #--------------------------------------------------------
-  test "get_scale returns the scale data" do
-    assert Transform.get_scale(@tx) == @scale
-  end
-
-  test "get_scale returns nil given nil transform" do
-    assert Transform.get_scale(nil) == nil
-  end
-
-  #--------------------------------------------------------
-  test "put_scale updates the scale data and calculates new final" do
-    {_,_,_,_,_,old_fin} = @tx
-    {_,_,3.3,_,_,new_fin} = Transform.put_scale(@tx, 3.3)
-    refute new_fin == old_fin
-  end
-
-
-  #============================================================================
-  # translation
-  
-  #--------------------------------------------------------
-  test "get_translation returns the translation data" do
-    assert Transform.get_translation(@tx) == @translate
-  end
-
-  test "get_translation returns nil given nil transform" do
-    assert Transform.get_translation(nil) == nil
-  end
-
-  #--------------------------------------------------------
-  test "put_translation updates the translation data and calculates new final" do
-    {_,_,_,_,_,old_fin} = @tx
-    {_,_,_,{1,2},_,new_fin} = Transform.put_translation(@tx, {1,2})
-    refute new_fin == old_fin
-  end
-
-
-  #============================================================================
-  # matrix
-  
-  #--------------------------------------------------------
-  test "get_matrix returns the matrix data" do
-    assert Transform.get_matrix(@tx) == @mx
-  end
-
-  test "get_matrix returns nil given nil transform" do
-    assert Transform.get_matrix(nil) == nil
-  end
-
-  #--------------------------------------------------------
-  test "put_matrix updates the matrix data and calculates new final" do
-    mx = MatrixBin.build_scale(1.01)
-    {_,_,_,_,_,old_fin} = @tx
-    {_,_,_,_,^mx,new_fin} = Transform.put_matrix(@tx, mx)
-    refute new_fin == old_fin
-  end
-
-
-  #============================================================================
-  # final transform matrix
-  
-  #--------------------------------------------------------
-  test "get_local returns the local matrix" do
-    {_,_,_,_,_,fin} = @tx
-    assert Transform.get_local(@tx) == fin
-  end
-
-  test "get_final returns nil given nil transform" do
-    assert Transform.get_local(nil) == nil
-  end
-
-  test "get_final calculates and returns the final if it is nil" do
-    {pin,rot,scl,trans,mx,fin} = @tx
-    assert Transform.get_local({pin,rot,scl,trans,mx,nil}) == fin
-  end
-
-
-  #============================================================================
-  # final transform matrix binary blob
-  
-  #--------------------------------------------------------
-  test "get_data returns the final binary blob" do
-    {_,_,_,_,_,fin} = @tx
-    assert Transform.get_data(@tx) == <<
-      @dflag    :: size(8),
-      @mx_code  :: size(8),
-      Utils.to_binary(fin, :row) :: binary
-    >>
-  end
-
-  test "get_data returns nil given nil" do
-    assert Transform.get_data(nil) == nil
-  end
-
-  test "get_data returns nil if everything is nil but the pin" do
-    assert Transform.get_data({{1,2},nil,nil,nil,nil,nil}) == nil
-  end
-
-  test "get_data calculates and returns the final blob if it is nil" do
-    {pin,rot,scl,trans,mx,fin} = @tx
-
-    assert Transform.get_data({pin,rot,scl,trans,mx,nil}) == <<
-      @dflag    :: size(8),
-      @mx_code  :: size(8),
-      Utils.to_binary(fin, :row) :: binary
-    >>
-  end
-
+  @tx           %{
+      pin:        @pin,
+      rotate:     @rotation,
+      scale:      @scale,
+      translate:  @translate,
+      matrix:     @mx
+    }
 
   #============================================================================
   # calculate the local matrix
@@ -259,9 +37,9 @@ defmodule Scenic.Primitive.TransformTest do
     assert Transform.calculate_local(nil) == nil
   end
 
-  test "calculate_local does nothing if only the pin is set" do
-    only_pin = {{1,2},nil,nil,nil,nil,nil}
-    assert Transform.calculate_local(only_pin) == only_pin
+  test "calculate_local returns nil if only pin is set" do
+    only_pin = %{pin: {10,20}}
+    assert Transform.calculate_local(only_pin) == nil
   end
 
   test "calculate_local calculates the local matrix in the right order" do
@@ -281,21 +59,8 @@ defmodule Scenic.Primitive.TransformTest do
       |> MatrixBin.mul( mx_scale )
       |> MatrixBin.mul( mx_inv_pin )
 
-    # build the matching incoming transform tuple by hand with nil final
-    tx = {
-      @pin,
-      @rotation,
-      @scale,
-      @translate,
-      @mx,
-      nil
-    }
-
     # calcualte the normal way
-    {_,_,_,_,_,local} = Transform.calculate_local( tx )
-
-    # check the answer
-    assert local == expected
+    assert Transform.calculate_local( @tx ) == expected
   end
 
 
