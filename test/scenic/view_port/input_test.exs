a64a491adb75588d3f127bb3430c39e1519023e0
Boyd Multerer <boyd@multerer.com>
2017-11-07 22:24:06 -0800

    get rid of really old tests to clear the way for the new

--- test/scenic/view_port/input_test.exs
+++ test/scenic/view_port/input_test.exs
@@ -30,63 +30,9 @@ defmodule Scenic.ViewPort.InputTest do
 
   #============================================================================
   # test helpers
-  defp test_context() do
-    Context.build(self(), self(), 123)
-  end
 
   defp set_up_registry() do
-    Registry.start_link(:duplicate, :scenic_input_registry)
   end
 
-  #============================================================================
-  # base mouse input handling
-
-  test "last mouse event recorded in the state" do
-    raw_event = <<
-      @mouse_opcode :: size(8),
-      5 :: size(8),
-      123 ::  unsigned-integer-size(32)-native,
-      10 ::    integer-size(16)-native,
-      20 ::    integer-size(16)-native
-    >>
-
-    mouse_state = Input.Mouse.build( 5, 123, 10, 20 )
-
-    set_up_registry()
-    state = Input.handle_raw_input(test_context(), %{}, raw_event)
-
-    assert Map.get(state, @input_key) == %{Input.Mouse => mouse_state}
-  end
-
-  #============================================================================
-  # key input handling
-
-  test "key down events are sent to the scene with uid of -1" do
-    key = 64
-
-    raw_event = <<
-      @key_opcode :: size(8),
-      @key_type_down :: size(8),
-      key ::  integer-size(16)-native
-    >>
-
-    assert Input.handle_raw_input(test_context(), %{}, raw_event) == %{}
-
-    assert_received( {:"$gen_cast", {:input, {:key_down, ^key}, -1} }  )
-  end
-
-  test "key up events are sent to the scene with uid of -1" do
-    key = 64
-
-    raw_event = <<
-      @key_opcode :: size(8),
-      @key_type_up :: size(8),
-      key ::  integer-size(16)-native
-    >>
-
-    assert Input.handle_raw_input(test_context(), %{}, raw_event) == %{}
-
-    assert_received( {:"$gen_cast", {:input, {:key_up, ^key}, -1} }  )
-  end
 
 end
