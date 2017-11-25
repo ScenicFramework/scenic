179724c3578df14b7039e03de5dc67e0178c6e35
Boyd Multerer <boyd@multerer.com>
2017-11-25 08:30:36 -0800

    get pulse working with multiple id matches

--- lib/scenic/animation/basic/pulse.ex
+++ lib/scenic/animation/basic/pulse.ex
@@ -13,30 +13,36 @@ defmodule Scenic.Animation.Basic.Pulse do
   #--------------------------------------------------------
   def tick( :step, graph, elapsed_ms, {id, factor, speed} ) do
     # this is the first time step is called. Save the original scale
-    [p] = Graph.get(graph, id)
+    orig_data = Graph.get(graph, id)
+    |> Enum.reduce(%{}, fn(%Primitive{uid: uid} = p, acc) ->
+      data = case Primitive.get_transform( p, :scale ) do
+        nil  -> {1.0, nil}
+        orig -> {orig, orig}
+      end
+      Map.put(acc, uid, data)
+    end)
 
-    {mid_point, original} = case Primitive.get_transform( p, :scale ) do
-      nil  -> {1.0, nil}
-      orig -> {orig, orig}
-    end
-
-    tick( :step, graph, elapsed_ms, {id, mid_point, factor, speed, original} )
+    tick( :step, graph, elapsed_ms, {id, factor, speed, orig_data} )
   end
 
   #--------------------------------------------------------
-  def tick( :step, graph, elapsed_ms, {id, mid_point, factor, speed, original_scale} ) do
-    scale = mid_point + :math.sin(elapsed_ms * speed) * factor
-    graph = Graph.modify(graph, id, fn(p) ->
+  def tick( :step, graph, elapsed_ms, {id, factor, speed, orig_data} ) do
+    graph = Graph.modify(graph, id, fn(%Primitive{uid: uid} = p) ->
+
+      {mid_point, _} = Map.get(orig_data, uid)
+      scale = mid_point + :math.sin(elapsed_ms * speed) * factor * mid_point
+
       Primitive.put_transform(p, :scale, scale)
     end)
-    {:continue, graph, {id, mid_point, factor, speed, original_scale} }
+    {:continue, graph, {id, factor, speed, orig_data} }
   end
 
   #--------------------------------------------------------
-  def tick( :stop, graph, elapsed_ms, {id, _, _, _, original_scale} ) do
+  def tick( :stop, graph, elapsed_ms, {id, _, _, orig_data} ) do
     # restore the original scale
     graph = Graph.modify(graph, id, fn(p) ->
-      Primitive.put_transform(p, :scale, original_scale)
+      {_, scale} = Map.get(orig_data, id)
+      Primitive.put_transform(p, :scale, scale)
     end)
     {:stop, graph }
   end
