ad6b462662405b57c2f0f35d42e3adc4a7e5fe8e
Boyd Multerer <boyd@multerer.com>
2017-11-26 07:56:34 -0800

    blink / hidden works

--- lib/scenic/animation/basic/blink.ex
+++ lib/scenic/animation/basic/blink.ex
@@ -8,28 +8,20 @@ defmodule Scenic.Animation.Basic.Blink do
   alias Scenic.Graph
   alias Scenic.Primitive
 
+  import IEx
 
-  def add_to_graph(graph, {id, period}, opts) when is_integer(period) do
-    super(graph, {id, period, true, 0}, opts)
+  @hz_to_rpms   2 * :math.pi / 1000
+
+  def add_to_graph(graph, {id, frequency}, opts) when is_number(frequency) do
+    super(graph, {id, frequency}, opts)
   end
 
-  def tick( :step, graph, elapsed_ms, {id, period, on, total_time} ) do
-    total_time = total_time + elapsed_ms
-
-    visible = (rem( total_time, period ) < (period / 2))
-
-    graph = if visible != on do
-      Graph.modify(graph, id, fn(p) ->
-        case visible do
-          true ->   Primitive.put_style( p, :hidden, false )
-          false ->  Primitive.put_style( p, :hidden, true )
-        end
-      end)
-    else
-      graph
-    end
-
-    {:continue, graph, {id, period, visible, total_time} }
+  def tick( :step, graph, elapsed_ms, {id, frequency} ) do
+    hidden = :math.sin(elapsed_ms * frequency * @hz_to_rpms) < 0
+    graph = Graph.modify(graph, id, fn(p) ->
+      Primitive.put_style( p, :hidden, hidden )
+    end)
+    {:continue, graph, {id, frequency} }
   end
 
 end
