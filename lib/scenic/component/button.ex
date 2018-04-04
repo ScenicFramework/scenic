3368b611cd2834d5e793b4a9c70b70c9eb24c724
Boyd Multerer <boyd@multerer.com>
2018-04-04 10:41:49 -0700

    try out demo with animation as the supervised scene

--- lib/scenic/component/button.ex
+++ lib/scenic/component/button.ex
@@ -44,32 +44,32 @@ defmodule Scenic.Component.Button do
 
 #  #--------------------------------------------------------
   def info() do
-    "#{IO.ANSI.red()}Button must be initialized with {{x,y}, text, message, opts}#{IO.ANSI.default_color()}\r\n" <>
+#    "#{IO.ANSI.red()}Button must be initialized with {{x,y}, text, message, opts}#{IO.ANSI.default_color()}\r\n" <>
+    "#{IO.ANSI.red()}Button must be initialized with {text, message, opts}#{IO.ANSI.default_color()}\r\n" <>
+    "Position the button with a transform\r\n" <>
     "The message will be sent to you in a click event when the button is used.\r\n" <>
     "Opts either a single or a list of option atoms\r\n" <>
     "One size indicator: :small, :normal, :large. :normal is the default\r\n" <>
     "One color indicator: :primary, :secondary, :success, :danger, :warning, :info, :light, :dark, :text\r\n" <>
     "Other modifiers: :outline\r\n" <>
-    "#{IO.ANSI.yellow()}Example: {{20,20}, \"Button\", :primary}\r\n" <>
-    "Example: {{20,20}, \"Outline\", [:danger, :outline]}\r\n" <>
-    "Example: {{20,20}, \"Large\", [:warning, :large]}\r\n" <>
-    "Example: {{20,20}, \"Small Outline\", [:info, :small, :outline]}#{IO.ANSI.default_color()}\r\n"
+    "#{IO.ANSI.yellow()}Example: {\"Button\", :primary}\r\n" <>
+    "Example: { \"Outline\", [:danger, :outline]}\r\n" <>
+    "Example: {\"Large\", [:warning, :large]}\r\n" <>
+    "Example: {\"Small Outline\", [:info, :small, :outline]}#{IO.ANSI.default_color()}\r\n"
   end
 
   #--------------------------------------------------------
-  def valid?( {point, text, msg} ), do: valid?( {point, text, msg, []} )
-  def valid?( {point, text, msg, opts} ) when is_atom(opts), do: valid?( {point, text, [opts]} )
+  def valid?( {text, msg} ), do: valid?( {text, msg, []} )
+  def valid?( {text, msg, opts} ) when is_atom(opts), do: valid?( {text, [opts]} )
 
-  def valid?( {{x, y}, text, msg, opts} ) when
-  is_number(x) and is_number(y) and
-  is_bitstring(text) and is_list(opts), do: true
+  def valid?( {text, msg, opts} ) when is_bitstring(text) and is_list(opts), do: true
   def valid?( d ), do: false
 
 
   #--------------------------------------------------------
-  def init( {{x, y}, text, msg} ), do: init( {{x, y}, text, msg, []} )
-  def init( {{x, y}, text, msg, opt} ) when is_atom(opt), do: init( {{x, y}, text, msg, [opt]} )
-  def init( {{x, y}, text, msg, opts} ) when is_list(opts) do
+  def init( {text, msg} ), do: init( {text, msg, []} )
+  def init( {text, msg, opt} ) when is_atom(opt), do: init( {text, msg, [opt]} )
+  def init( {text, msg, opts} ) when is_list(opts) do
     # get the size
     size_opt = Enum.find(opts, &Enum.member?(@valid_sizes, &1) ) || :normal
     color_opt = Enum.find(opts, &Enum.member?(@valid_colors, &1) ) || :primary
@@ -81,9 +81,9 @@ defmodule Scenic.Component.Button do
     {width, hieght, radius, font_size} = @sizes[size_opt]
 
     graph = Graph.build( font: {:roboto, font_size} )
-    |> Primitive.RoundedRectangle.add_to_graph( {{x,y}, width, hieght, radius},
+    |> Primitive.RoundedRectangle.add_to_graph( {{0,0}, width, hieght, radius},
       color: button_color, id: :btn )
-    |> Primitive.Text.add_to_graph( {{x+8,y+(hieght*0.7)}, text}, color: text_color )
+    |> Primitive.Text.add_to_graph( {{8,(hieght*0.7)}, text}, color: text_color )
 
     state = %{
       graph: graph,
