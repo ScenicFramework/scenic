8c79777e52c0f047bdad2c164b90e0ebc89abece
Boyd Multerer <boyd@multerer.com>
2018-06-18 20:09:54 -0700

    update font tests

---
+++ test/scenic/primitive/style/font_test.exs
@@ -0,0 +1,39 @@
+#
+#  Created by Boyd Multerer on 5/12/17.
+#  Copyright © 2017 Kry10 Industries. All rights reserved.
+#
+
+defmodule Scenic.Primitive.Style.FontTest do
+  use ExUnit.Case, async: true
+  doctest Scenic
+
+  alias Scenic.Primitive.Style
+  alias Scenic.Primitive.Style.Font
+
+
+  #============================================================================
+  # verify - various forms
+
+  test "verfy works" do
+    assert Font.verify( :roboto )
+    assert Font.verify( "98u2r8hkajhfs" )
+  end
+
+  test "verify rejects bad values" do
+    refute Font.verify( 123 )
+  end
+
+  test "verify! works" do
+    assert Font.verify!( :roboto )
+    assert Font.verify!( "98u2r8hkajhfs" )
+  end
+
+  test "verify! raises an error" do
+    assert_raise Style.FormatError, fn ->
+      Font.verify!( 123 )
+    end
+  end
+
+end
+
+
