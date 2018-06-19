96ed4e279c72c70749b1789444d6bca0142e8cda
Boyd Multerer <boyd@multerer.com>
2018-06-18 20:04:05 -0700

    add scissor style test

---
+++ test/scenic/primitive/style/scissor_test.exs
@@ -0,0 +1,38 @@
+#
+#  Created by Boyd Multerer on June 18, 2018.
+#  Copyright © 2018 Kry10 Industries. All rights reserved.
+#
+
+defmodule Scenic.Primitive.Style.ScissorTest do
+  use ExUnit.Case, async: true
+  doctest Scenic
+
+  alias Scenic.Primitive.Style
+  alias Scenic.Primitive.Style.Scissor
+
+
+  #============================================================================
+  # verify - various forms
+
+  test "verfy works" do
+    assert Scissor.verify( {{10,20}, 100, 200} )
+  end
+
+  test "verify rejects invalid values" do
+    refute Scissor.verify( 123 )
+    refute Scissor.verify( {10, 20, 100, 200} )
+  end
+
+  test "verify! works" do
+    assert Scissor.verify!( {{10,20}, 100, 200} )
+  end
+
+  test "verify! raises an error" do
+    assert_raise Style.FormatError, fn ->
+      Scissor.verify!( 123 )
+    end
+  end
+
+end
+
+
