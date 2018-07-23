14f23d1fa9225b0d388cfcd4f82c62277d97288f
Boyd Multerer <boyd@multerer.com>
2018-07-23 15:42:32 -0700

    add main math typespecs

---
+++ lib/math.ex
@@ -0,0 +1,23 @@
+#
+#  Copyright © 2017 Boyd Multerer. All rights reserved.
+#
+
+defmodule Scenic.Math do
+  @moduledoc """
+  Documentation for Scenic.Math.
+  """
+
+
+  @type vector_2 :: {x :: number, y :: number}
+  @type vector_3 :: {x :: number, y :: number, z :: number}
+  @type vector_4 :: {x :: number, y :: number, z :: number, w :: number}
+
+  @type point :: {x :: number, y :: number}
+
+  @type line :: {p0 :: point, p1 :: point}
+  @type triangle :: {p0 :: point, p1 :: point, p2 :: point}
+  @type quad :: {p0 :: point, p1 :: point, p2 :: point, p3 :: point}
+
+  @type matrix :: <<_::64>>
+
+end
