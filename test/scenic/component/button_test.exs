8bfa249256fcbb3878fb473aa3b674bb1a01ef26
Boyd Multerer <boyd@multerer.com>
2018-09-18 21:56:07 -0500

    run formatter

--- test/scenic/component/button_test.exs
+++ test/scenic/component/button_test.exs
@@ -54,72 +54,91 @@ defmodule Scenic.Component.ButtonTest do
 
   test "init works with various alignments" do
     {:ok, state} = Button.init("Button", [])
-    %Primitive{styles: %{text_align: :center}} = Graph.get!( state.graph, :title )
+    %Primitive{styles: %{text_align: :center}} = Graph.get!(state.graph, :title)
 
     {:ok, state} = Button.init("Button", styles: %{alignment: :left}, id: :button_id)
-    %Primitive{styles: %{text_align: :left}} = Graph.get!( state.graph, :title )
+    %Primitive{styles: %{text_align: :left}} = Graph.get!(state.graph, :title)
 
     {:ok, state} = Button.init("Button", styles: %{alignment: :center}, id: :button_id)
-    %Primitive{styles: %{text_align: :center}} = Graph.get!( state.graph, :title )
+    %Primitive{styles: %{text_align: :center}} = Graph.get!(state.graph, :title)
 
     {:ok, state} = Button.init("Button", styles: %{alignment: :right}, id: :button_id)
-    %Primitive{styles: %{text_align: :right}} = Graph.get!( state.graph, :title )
+    %Primitive{styles: %{text_align: :right}} = Graph.get!(state.graph, :title)
   end
 
   # ============================================================================
   # handle_input
 
   test "handle_input {:cursor_enter, _uid} sets contained" do
-    {:noreply, state} = Button.handle_input( {:cursor_enter, 1}, %{}, %{@state | pressed: true} )
+    {:noreply, state} = Button.handle_input({:cursor_enter, 1}, %{}, %{@state | pressed: true})
     assert state.contained
     # confirm the graph was pushed
-    assert_receive( {:"$gen_cast", {:push_graph, _, _, _}} )
+    assert_receive({:"$gen_cast", {:push_graph, _, _, _}})
   end
 
   test "handle_input {:cursor_exit, _uid} clears contained" do
-    {:noreply, state} = Button.handle_input( {:cursor_exit, 1}, %{}, %{@state | pressed: true} )
+    {:noreply, state} = Button.handle_input({:cursor_exit, 1}, %{}, %{@state | pressed: true})
     refute state.contained
     # confirm the graph was pushed
-    assert_receive( {:"$gen_cast", {:push_graph, _, _, _}} )
+    assert_receive({:"$gen_cast", {:push_graph, _, _, _}})
   end
 
   test "handle_input {:cursor_button, :press" do
     context = %ViewPort.Context{viewport: self()}
-    {:noreply, state} = Button.handle_input( {:cursor_button, {:left, :press, nil, nil}}, context, %{@state | pressed: false, contained: true} )
+
+    {:noreply, state} =
+      Button.handle_input({:cursor_button, {:left, :press, nil, nil}}, context, %{
+        @state
+        | pressed: false,
+          contained: true
+      })
+
     assert state.pressed
 
     # confirm the input was captured
-    assert_receive( {:"$gen_cast", {:capture_input, ^context, [:cursor_button, :cursor_pos]}} )
+    assert_receive({:"$gen_cast", {:capture_input, ^context, [:cursor_button, :cursor_pos]}})
     # confirm the graph was pushed
-    assert_receive( {:"$gen_cast", {:push_graph, _, _, _}} )
+    assert_receive({:"$gen_cast", {:push_graph, _, _, _}})
   end
 
   test "handle_input {:cursor_button, :release" do
     context = %ViewPort.Context{viewport: self()}
-    {:noreply, state} = Button.handle_input( {:cursor_button, {:left, :release, nil, nil}}, context, %{@state | pressed: true, contained: true} )
+
+    {:noreply, state} =
+      Button.handle_input({:cursor_button, {:left, :release, nil, nil}}, context, %{
+        @state
+        | pressed: true,
+          contained: true
+      })
+
     refute state.pressed
 
     # confirm the input was released
-    assert_receive( {:"$gen_cast", {:release_input, [:cursor_button, :cursor_pos]}} )
+    assert_receive({:"$gen_cast", {:release_input, [:cursor_button, :cursor_pos]}})
 
     # confirm the graph was pushed
-    assert_receive( {:"$gen_cast", {:push_graph, _, _, _}} )
+    assert_receive({:"$gen_cast", {:push_graph, _, _, _}})
   end
-
 
   test "handle_input {:cursor_button, :release sends a message if contained" do
     self = self()
     Process.put(:parent_pid, self)
     context = %ViewPort.Context{viewport: self()}
-    {:noreply, _} = Button.handle_input( {:cursor_button, {:left, :release, nil, nil}}, context, %{@state | pressed: true, contained: true} )
+
+    {:noreply, _} =
+      Button.handle_input({:cursor_button, {:left, :release, nil, nil}}, context, %{
+        @state
+        | pressed: true,
+          contained: true
+      })
 
     # confirm the graph was pushed
-    assert_receive( {:"$gen_cast", {:event, {:click, :test_id}, ^self}} )
+    assert_receive({:"$gen_cast", {:event, {:click, :test_id}, ^self}})
   end
 
   test "handle_input does nothing on unknown input" do
     context = %ViewPort.Context{viewport: self()}
-    {:noreply, state} = Button.handle_input( :unknown, context, @state )
+    {:noreply, state} = Button.handle_input(:unknown, context, @state)
     assert state == @state
   end
 end
