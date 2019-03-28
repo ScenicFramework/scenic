d51a480316d27ce7dbbc77365115420ae7671c56
Boyd Multerer <boyd@multerer.com>
2019-03-28 16:51:35 +1300

    remove dead code and minor cleanup

--- lib/scenic/graph.ex
+++ lib/scenic/graph.ex
@@ -341,14 +341,13 @@ defmodule Scenic.Graph do
 
   If multiple groups have the given `id`, then the builder is run against each of them.
   """
-
   def add_to(%__MODULE__{primitives: prims} = graph, id, builder) when is_function(builder, 1) do
     # resolve the id into a list of uids
     graph
     |> resolve_id(id)
     |> Enum.reduce(graph, fn uid, g ->
       case prims[uid] do
-        %Primitive{module: Group} = p ->
+        %Primitive{module: Group} ->
           g
           |> Map.put(:add_to, uid)
           |> builder.()
@@ -362,39 +361,6 @@ defmodule Scenic.Graph do
       end
     end)
   end
-
-  # def add_to( %Graph{} = graph, id, p ) when not is_integer(id) do
-  #   get_by_uid(graph, id)
-  #   |> Enum.reduce( graph, fn(uid, g) ->
-  #     case get_by_uid(g, uid) do
-  #       %Primitive{module: Group} ->
-  #         {graph, _uid} = insert_at({g, uid}, -1, p)
-  #         graph
-  #       _ ->
-  #         raise @err_msg_group
-  #     end
-  #   end)
-  # end
-
-  # build and add a new primitive to an existing group in a graph
-  # def add_to( graph, id, primitive_module, primitive_data, opts \\ [])
-  # def add_to( %Graph{add_to: puid} = graph, id, primitive_module, primitive_data, opts) when not is_integer(id) do
-  #   get_id(graph, id)
-  #   |> Enum.reduce( graph, fn(uid, g) ->
-  #     case get_by_uid(graph, uid) do
-  #       %Primitive{module: Group} ->
-  #         g
-  #         # set the new group as the add_to target
-  #         |> Map.put(:add_to, uid)
-  #         # add the new primitive
-  #         |> add( primitive_module, primitive_data, opts )
-  #       _ ->
-  #         raise @err_msg_group
-  #     end
-  #   end)
-  #   # restore the add_to back to whatever it was before
-  #   |> Map.put(:add_to, puid)
-  # end
 
   # ============================================================================
   # put an element by uid - internal use
