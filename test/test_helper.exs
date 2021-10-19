d58c577a2a00c85c37501287459187ab9ae289f2
Boyd Multerer <boyd@kry10.com>
2021-10-19 20:22:47 +1300

    Clean up old - unused - config from the test helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,4 @@
 # dynamically update the config to point to the test assets
-Application.put_env(:scenic, :assets,
-  module: Scenic.Test.Assets,
-  alias: [
-    roboto_mono: "fonts/roboto_mono.ttf",
-    test_roboto: "fonts/roboto.ttf",
-    test_parrot: "images/parrot.png"
-  ]
-)
+Application.put_env(:scenic, :assets, module: Scenic.Test.Assets )
 
 ExUnit.start()
