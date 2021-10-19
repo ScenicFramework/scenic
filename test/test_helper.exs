dc056f01f525de196af902b2d80c4191786c1e84
Boyd Multerer <boyd@kry10.com>
2021-10-19 20:23:26 +1300

    mix format

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
 # dynamically update the config to point to the test assets
-Application.put_env(:scenic, :assets, module: Scenic.Test.Assets )
+Application.put_env(:scenic, :assets, module: Scenic.Test.Assets)
 
 ExUnit.start()
