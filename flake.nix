32de2a7e482a0473da0552d67522782d33ca6d4c
crertel <chris@kedagital.com>
2024-05-04 22:47:53 -0500

    Move to elixir 1.16 for develop flake, nix 23.11.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for building scenic.";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.05"; };
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
     flake-utils = { url = "github:numtide/flake-utils"; };
   };
   outputs = { self, nixpkgs, flake-utils }:
@@ -17,7 +17,7 @@
     {
       devShell = pkgs.mkShell {
         buildInputs = [
-          elixir
+          elixir_1_16
           elixir_ls
         ] ++ optional stdenv.isLinux inotify-tools
           ++ optional stdenv.isDarwin terminal-notifier
